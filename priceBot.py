980f2e4fcd81a452f298b4299a946122526fab59
Jordan <jordan.dwo@gmail.com>
2017-07-06 17:42:43 -0700

    restructured try statement, and made KeyError exception handler

--- priceBot.py
+++ priceBot.py
@@ -278,50 +278,50 @@ if __name__ == "__main__":
         cfg = yaml.safe_load(ymlfile)
 
     while True:
-
-        now = time.time()
-        round(now)
-
-        #forces tweet to initiate on the hour
-        while now % time_to_tweet > EPSILON:
-            print('Waiting to tweet.')
+        try:
             now = time.time()
             round(now)
-            time.sleep(MINUTE / 2)
 
-        for bot in cfg:
-            consumer_key = cfg[bot]['consumer_key']
-            consumer_secret = cfg[bot]['consumer_secret']
-            access_key = cfg[bot]['access_key']
-            access_secret = cfg[bot]['access_secret']
-            coin_name = cfg[bot]['coin_name']
-            full_name = cfg[bot]['full_name']
-            download_folder = _findDownLoadsFolder()
-            increasing_color = cfg[bot]['increasing_color']
-            decreasing_color = cfg[bot]['decreasing_color']
+            #forces tweet to initiate on the hour
+            while now % time_to_tweet > EPSILON:
+                print('Waiting to tweet.')
+                now = time.time()
+                round(now)
+                time.sleep(MINUTE / 2)
 
-            print(cfg)
+            for bot in cfg:
+                consumer_key = cfg[bot]['consumer_key']
+                consumer_secret = cfg[bot]['consumer_secret']
+                access_key = cfg[bot]['access_key']
+                access_secret = cfg[bot]['access_secret']
+                coin_name = cfg[bot]['coin_name']
+                full_name = cfg[bot]['full_name']
+                download_folder = _findDownLoadsFolder()
+                increasing_color = cfg[bot]['increasing_color']
+                decreasing_color = cfg[bot]['decreasing_color']
 
-            auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-            auth.set_access_token(access_key, access_secret)
-            api = tweepy.API(auth)
+                print(cfg)
 
-            bot = PriceBot(consumer_key, consumer_secret, access_key,
-                access_secret, coin_name, full_name, download_folder,
-                increasing_color, decreasing_color)
+                auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+                auth.set_access_token(access_key, access_secret)
+                api = tweepy.API(auth)
 
-            try:
+                bot = PriceBot(consumer_key, consumer_secret, access_key,
+                    access_secret, coin_name, full_name, download_folder,
+                    increasing_color, decreasing_color)
+
                 bot.plotTweet()
                 bot.updateTweet()
 
-                time.sleep( time_to_tweet / 2 )
-                #clears chrome window to avoid openning too many tabs and crashing system
-                if browser == 'chrome.exe':
-                    Popen(['taskkill ', '/F',  '/IM', browser], shell=False)
-                elif browser == 'chromium-browser':
-                    os.system('killall ' + browser)
-                else:
-                    print('Cannot find browser to kill.')
-                    print(browser)
-            except Exception as execption:
-                print(execption)
+        except KeyError as err:
+            print(err)
+        finally:
+            time.sleep( time_to_tweet / 2 )
+            #clears chrome window to avoid openning too many tabs and crashing system
+            if browser == 'chrome.exe':
+                Popen(['taskkill ', '/F',  '/IM', browser], shell=False)
+            elif browser == 'chromium-browser':
+                os.system('killall ' + browser)
+            else:
+                print('Cannot find browser to kill.')
+                print(browser)
